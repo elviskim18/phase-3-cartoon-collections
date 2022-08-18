152510bbfdb9bc99ebba6cf6bc2db3ecdc168e2a
Kimani <elvis.kimani@student.moringaschool.com>
2022-08-18 16:33:58 +0300

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,32 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves (array)
   # Your code here
+  newarray = []
+  array.each_with_index do |name,index|
+    puts newarray.push("/#{index+1}.*#{name}/")
+  end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet (array)
   # Your code here
+  array_cap = []
+  array.each do |name|
+    array_cap.push (name.capitalize + "!")
+  end
+  array_cap
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)
   # Your code here
+  array.any?{|elem| elem.length > 4 }
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  thereis = array.detect("cheddar" || "gouda" || "camembert")
+  inc = array.include?("cheddar" || "gouda" || "camembert")
+  if inc
+    return array.detect{|elem| elem==("cheddar" || "gouda" || "camembert")}
+  end
+  puts inc
 end
